9754ef6c26be96ef6f65f75e71c76baf992d4fd0
hungperfin <hungnn2810@gmail.com>
2023-07-11 00:00:43 +0700

    inti

--- Main.cs
+++ Main.cs
@@ -12,9 +12,9 @@ using System.Windows.Forms;
 
 namespace CrawlData
 {
-    public partial class Form1 : Form
+    public partial class Main : Form
     {
-        public Form1()
+        public Main()
         {
             InitializeComponent();
         }
